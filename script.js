108d2a66aa42b42e09e5eec9a581d9f9fa93a904
fs820 <241308@st.yoshida-g.ac.jp>
2025-04-13 11:25:04 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// メッセージ
 let ButtonMessage = "まひろ"
 
+//----------------------------
+// メッセージを呼び出す関数
+//----------------------------
 function ButtonClick() 
 {
+    // メッセージ表示
     alert(ButtonMessage);
 }
